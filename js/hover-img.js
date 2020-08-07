a56c08cfb8f693d54aa0ea0930b8d2d2e473e236
nhan123615 <nhan123615@users.noreply.github.com>
2020-08-07 22:15:11 +0700

    10h14 07/08/20
    10h14 07/08/20

---
+++ js/hover-img.js
@@ -0,0 +1,59 @@
+
+
+
+$(document).ready(function(){
+
+/* add class hover img */
+    $(".post-entry-1").addClass("hovereffect");
+    $(".img-bg").addClass("hovereffect");
+    $(".thumbnail").addClass("hovereffect");   
+
+/* add class hover img */
+
+/* function hoverImg(classImg, hoverClass){
+    $(classImg).mouseover(function(){
+        $(hoverClass).css({
+            '-webkit-transition' : '.3s all ease',
+            '-o-transition': '.3s all ease',
+            'transition': ' .3s all ease',
+            'color': '#8bc34a'
+        })
+    }).mouseout(function() {
+            $(hoverClass).css({
+                'color': '#000'
+            })
+        });
+}
+
+hoverImg(".half-post-entry img"," .half-post-entry  .contents h2 a")
+hoverImg(".post-entry-1 img","  .post-entry-1 h2 a")
+hoverImg(".post-entry-2 img","  .post-entry-2 > h2 a") */
+
+
+
+
+/* $(".half-post-entry img").mouseover(function(){
+    $(" .half-post-entry  .contents h2 a").css({
+        '-webkit-transition' : '.3s all ease',
+        '-o-transition': '.3s all ease',
+        'transition': ' .3s all ease',
+        'color': '#8bc34a'
+    })
+}).mouseout(function() {
+        $(" .half-post-entry  .contents h2 a").css({
+            'color': '#000'
+        })
+    }); */
+
+
+
+
+
+
+})
+
+
+
+
+
+
