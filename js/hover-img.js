4a933b74e06160532c86250403cd2ae71c33abc1
nhan123615 <64056052+nhan123615@users.noreply.github.com>
2020-08-08 11:28:48 +0700

    08.08.20 11:28AM
    08.08.20 11:28AM

--- js/hover-img.js
+++ js/hover-img.js
@@ -10,22 +10,37 @@ $(document).ready(function(){
 
 /* add class hover img */
 
-/* function hoverImg(classImg, hoverClass){
+function hover(classImg, hoverClass){
     $(classImg).mouseover(function(){
         $(hoverClass).css({
-            '-webkit-transition' : '.3s all ease',
+            'opacity' : '0.3',        
+            '-webkit-transition': '.3s all ease',
             '-o-transition': '.3s all ease',
-            'transition': ' .3s all ease',
-            'color': '#8bc34a'
+             'transition': '.3s all ease',
+            
         })
     }).mouseout(function() {
             $(hoverClass).css({
-                'color': '#000'
+                'opacity' : '0',
+                '-webkit-transition': '.3s all ease',
+                '-o-transition': '.3s all ease',
+                 'transition': '.3s all ease'
             })
         });
 }
 
-hoverImg(".half-post-entry img"," .half-post-entry  .contents h2 a")
+/* hover prev,next */
+
+hover(".hero-slide",".owl-slide-3 .owl-nav .owl-prev, .owl-slide .owl-nav .owl-prev, .owl-style .owl-nav .owl-prev")
+
+hover(".hero-slide",".owl-slide-3 .owl-nav .owl-next, .owl-slide .owl-nav .owl-next, .owl-style .owl-nav .owl-next")
+/* hover prev,next  end*/
+
+
+
+
+
+/* hoverImg(".half-post-entry img"," .half-post-entry  .contents h2 a")
 hoverImg(".post-entry-1 img","  .post-entry-1 h2 a")
 hoverImg(".post-entry-2 img","  .post-entry-2 > h2 a") */
 
